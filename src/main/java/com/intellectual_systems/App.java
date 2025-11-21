4c5666c5d95e2d87955ac0c20eba67e80bec6267
jjdlabee <jjdlabee@gmail.com>
2025-11-21 15:52:34 -0400

    first commit

---
+++ src/main/java/com/intellectual_systems/App.java
@@ -0,0 +1,13 @@
+package com.intellectual_systems;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
