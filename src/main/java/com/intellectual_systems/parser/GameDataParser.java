9ee2cc6b741d1fe80c9853825c91c503a9e3e9d5
jjdlabee <jjdlabee@gmail.com>
2025-11-24 13:52:10 -0400

    employing template model

---
+++ src/main/java/com/intellectual_systems/parser/GameDataParser.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+package com.intellectual_systems.parser;
+
+/**
+ *
+ * @author Jonathan
+ */
+public interface GameDataParser {
+    void parse(String filePath);
+}
